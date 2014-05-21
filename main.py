5f7c37d3fdc556c53830b761f3d63f65310fa127
Yuri Silva <yuryjefse@gmail.com>
2014-05-21 14:23:25 -0300

    customizando um widget e usando import no arquivo kv e ListItemButton e ListAdapter

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.network.urlrequest import UrlRequest
 
+class WeatherRoot(BoxLayout):
+        pass
+
 class AddLocationForm(BoxLayout):
         search_input = ObjectProperty()
         
         def search_location(self):
-                print("entrei")
                 search_template = "http://api.openweathermap.org/data/2.5/find?q={}&type=like"
                 search_url = search_template.format(self.search_input.text)
                 request = UrlRequest(search_url, self.found_location)
@@ -18,6 +20,9 @@ class AddLocationForm(BoxLayout):
                 cities = ["{} ({})".format(d['name'], d['sys']['country'])
                           for d in data['list']]
                 self.search_results.item_strings = cities
+                self.search_results.adapter.data.clear()
+                self.search_results.adapter.data.extend(cities)
+                self.search_results._trigger_reset_populate()
 
 class WeatherApp(App):
         pass
