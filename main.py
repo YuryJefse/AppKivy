1e9a11b2b21eaf08298821fac5f142f8705dcacc
Yury Silva <yuryjefse@gmail.com>
2014-05-22 18:47:52 -0300

    Pegando mais dados do webservice como condicoes do tempo e temperatura maxima e minima. E feito ajustes na tela de location

--- main.py
+++ main.py
@@ -2,12 +2,31 @@ import json
 from kivy.app import App
 from kivy.factory import Factory
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
 from kivy.uix.listview import ListItemButton
 from kivy.network.urlrequest import UrlRequest
+from kivy.properties import ObjectProperty, ListProperty, StringProperty, NumericProperty
+
+class CurrentWeather(BoxLayout):
+        location = ListProperty(['New York', 'US'])
+        conditions = StringProperty()
+        temp = NumericProperty()
+        temp_min = NumericProperty()
+        temp_max = NumericProperty()
+
+        def update_weather(self):
+                weather_template = "http://api.openweathermap.org/data/2.5/weather?q={},{}&units=metric"
+                weather_url = weather_template.format(*self.location)
+                request = UrlRequest(weather_url, self.weather_retrieved)
+        
+        def weather_retrieved(self, request, data):
+                data = json.loads(data.decode()) if not isinstance(data, dict) else data
+                self.conditions = data['weather'][0]['description']
+                self.temp = data['main']['temp']
+                self.temp_min = data['main']['temp_min']
+                self.temp_max = data['main']['temp_max']
 
 class LocationButton(ListItemButton):
-        pass
+        location = ListProperty()
 
 class WeatherRoot(BoxLayout):
         current_weather = ObjectProperty()
@@ -15,11 +34,13 @@ class WeatherRoot(BoxLayout):
         def show_current_weather(self, location=None):
                 self.clear_widgets()
 
-                if location is None and self.current_weather is None:
-                        location = "New York (US)"
+                if self.current_weather is None:
+                        self.current_weather = CurrentWeather()
+                
                 if location is not None:
-                        self.current_weather = Factory.CurrentWeather()
                         self.current_weather.location = location
+
+                self.current_weather.update_weather()
                 self.add_widget(self.current_weather)
                 
         def show_add_location_form(self):
@@ -42,6 +63,13 @@ class AddLocationForm(BoxLayout):
                 self.search_results.adapter.data.clear()
                 self.search_results.adapter.data.extend(cities)
                 self.search_results._trigger_reset_populate()
+                
+        def args_converter(self, index, data_item):
+                city = data_item.split(" (")[0]
+                country = data_item.split(" (")[1][0:-1]
+                print(data_item)
+                
+                return {'location': (city, country)}
 
 class WeatherApp(App):
         pass
